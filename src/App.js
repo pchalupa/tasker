cecd0e77c8f0d4ded876bed11f0b0a019fab91b0
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-04-01 21:03:42 +0200

    Removes unecessered div.

--- src/App.js
+++ src/App.js
@@ -3,32 +3,29 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Tasker from './screens/Tasker';
 import Admin from './screens/Admin';
 import { ROUTES } from './constants/routes';
-import './App.css';
 
 function App() {
     return (
         <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to={ROUTES.ROOT}>Úkolníček</Link>
-                        </li>
-                        <li>
-                            <Link to={ROUTES.ADMIN}>Administrace</Link>
-                        </li>
-                    </ul>
-                </nav>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to={ROUTES.ROOT}>Úkolníček</Link>
+                    </li>
+                    <li>
+                        <Link to={ROUTES.ADMIN}>Administrace</Link>
+                    </li>
+                </ul>
+            </nav>
 
-                <Switch>
-                    <Route exact path={ROUTES.ROOT}>
-                        <Tasker />
-                    </Route>
-                    <Route path={ROUTES.ADMIN}>
-                        <Admin />
-                    </Route>
-                </Switch>
-            </div>
+            <Switch>
+                <Route exact path={ROUTES.ROOT}>
+                    <Tasker />
+                </Route>
+                <Route path={ROUTES.ADMIN}>
+                    <Admin />
+                </Route>
+            </Switch>
         </Router>
     );
 }
