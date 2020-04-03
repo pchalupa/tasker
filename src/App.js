3df1d6c326ed8171d95c0cb0e4ab3fd62db0667f
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-04-03 22:00:36 +0200

    Removes links

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Tasker from './screens/Tasker';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Students from './screens/StudentsScreen';
+import Tasks from './screens/TasksScreen';
 import Admin from './screens/Admin';
 import { ROUTES } from './constants/routes';
 
 function App() {
     return (
         <Router>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to={ROUTES.ROOT}>Úkolníček</Link>
-                    </li>
-                    <li>
-                        <Link to={ROUTES.ADMIN}>Administrace</Link>
-                    </li>
-                </ul>
-            </nav>
-
             <Switch>
-                <Route exact path={ROUTES.ROOT}>
-                    <Tasker />
-                </Route>
-                <Route path={ROUTES.ADMIN}>
-                    <Admin />
-                </Route>
+                <Route exact path={ROUTES.STUDENTS} component={Students} />
+                <Route exact path={ROUTES.TASKS} component={Tasks} />
+                <Route path={ROUTES.ADMIN} component={Admin} />
             </Switch>
         </Router>
     );
