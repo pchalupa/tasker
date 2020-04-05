4f983b5aa351e107bb85766dc65d5cd0ac9d7f3a
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-04-05 21:59:34 +0200

    Removes unecesary code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Students from './screens/StudentsScreen';
-import Tasks from './screens/TasksScreen';
-import Admin from './screens/Admin';
-import { ROUTES } from './constants/routes';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path={ROUTES.STUDENTS} component={Students} />
-                <Route exact path={ROUTES.TASKS} component={Tasks} />
-                <Route path={ROUTES.ADMIN} component={Admin} />
-            </Switch>
-        </Router>
-    );
+    return <Navigation />;
 }
 
 export default App;
