182e4dfe1be65736ad968e860dfe94337d1a9b9d
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-04-05 21:59:51 +0200

    Firebase config

---
+++ src/storage/Firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: 'AIzaSyBto1cuKtwUZONzQ4PBt7zTpvqWkgep8aQ',
+    authDomain: 'tasker-926bd.firebaseapp.com',
+    databaseURL: 'https://tasker-926bd.firebaseio.com',
+    projectId: 'tasker-926bd',
+    storageBucket: 'tasker-926bd.appspot.com',
+    messagingSenderId: '815411434227',
+    appId: '1:815411434227:web:a4309274c0dd75baaadd86',
+    measurementId: 'G-LMFR2XGNDL'
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth;
+export const db = firebase.firestore();
