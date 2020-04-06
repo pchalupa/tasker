521d060aa785d82f49b72bfbb9ad1f86b76ef87a
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-04-06 20:27:49 +0200

    Tags style edit

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -5,20 +5,15 @@
 import React from 'react';
 import styles from '../styles/Tags.module.scss';
 
-class Tags extends React.PureComponent {
-    render() {
-        return (
-            <div className={ styles.tagsContainer }>
-                {this.props.tags &&
-                    this.props.tags.map((tag, index) => (
-                        <span
-                            className={ styles.tag }
-                            key={ index }
-                        >{`#${ tag }`}</span>
-                    ))}
-            </div>
-        );
-    }
+function Tags(props) {
+    return (
+        <div className={styles.container}>
+            {props.tags &&
+                props.tags.map((tag, index) => (
+                    <span className={styles.tag} key={index}>{`#${tag}`}</span>
+                ))}
+        </div>
+    );
 }
 
 export default Tags;
