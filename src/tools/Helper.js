66765f7b4d2629a1a805ac9dc98dfee71194a3f1
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-04-12 20:06:46 +0200

    Adjust returns date

---
+++ src/tools/Helper.js
@@ -0,0 +1,20 @@
+/**
+ * @module Helper
+ */
+
+/**
+ * Returns date start and end of current week.
+ *
+ * @returns {object} start, end
+ */
+export default function getCurrentWeek() {
+    const current = new Date(2020, 3, 6);
+    current.setHours(0, 0, 0, 0);
+    const start =
+        current.getDate() - (current.getDay() === 0 ? 6 : current.getDay() - 1);
+    const end = start + 7;
+    return {
+        start: new Date(current.setDate(start)),
+        end: new Date(current.setDate(end))
+    };
+}
