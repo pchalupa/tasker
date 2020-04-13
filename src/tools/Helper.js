0ccdfaa97f20f8a3f6b41968400b40a05d24dcbb
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-04-13 13:50:39 +0200

    Adjust getCurrentWeek method

--- src/tools/Helper.js
+++ src/tools/Helper.js
@@ -8,11 +8,11 @@
  * @returns {object} start, end
  */
 export default function getCurrentWeek() {
-    const current = new Date(2020, 3, 6);
+    const current = new Date();
     current.setHours(0, 0, 0, 0);
     const start =
         current.getDate() - (current.getDay() === 0 ? 6 : current.getDay() - 1);
-    const end = start + 7;
+    const end = start + 6;
     return {
         start: new Date(current.setDate(start)),
         end: new Date(current.setDate(end))
