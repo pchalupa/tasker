435cbab29d72eee5c245ba7fba56f032dff01d56
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-04-15 21:30:51 +0200

    Move config to ENV file

--- src/storage/Firebase.js
+++ src/storage/Firebase.js
@@ -5,14 +5,14 @@ import 'firebase/performance';
 import 'firebase/analytics';
 
 firebase.initializeApp({
-    apiKey: 'AIzaSyBto1cuKtwUZONzQ4PBt7zTpvqWkgep8aQ',
-    authDomain: 'tasker-926bd.firebaseapp.com',
-    databaseURL: 'https://tasker-926bd.firebaseio.com',
-    projectId: 'tasker-926bd',
-    storageBucket: 'tasker-926bd.appspot.com',
-    messagingSenderId: '815411434227',
-    appId: '1:815411434227:web:a4309274c0dd75baaadd86',
-    measurementId: 'G-LMFR2XGNDL'
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 });
 
 const base = firebase;
