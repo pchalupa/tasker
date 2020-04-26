aa1b1401f2f67726e772b35c3d27163c97d37c75
Petr Chalupa <chalupa.petr93@gmail.com>
2020-04-26 17:44:10 +0200

    Move router to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import Navigation from './components/Navigation/Navigation';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import StudentsScreen from './screens/StudentsScreen';
+import TasksScreen from './screens/TasksScreen';
+import AdminScreen from './screens/AdminScreen';
+import { ROUTES } from './constants/routes';
 
 function App() {
-    return <Navigation />;
+	return (
+		<Router>
+			<Switch>
+				<Route exact path={ROUTES.ROOT} component={StudentsScreen} />
+				<Route exact path={ROUTES.TASKS} component={TasksScreen} />
+				<Route exact path={ROUTES.ADMIN} component={AdminScreen} />
+				<Route path="*">
+					<Redirect to={ROUTES.ROOT} />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
