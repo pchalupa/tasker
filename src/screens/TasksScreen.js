32560f6005875e1b92963339a4d28edb29dd78dc
Petr Chalupa <chalupa.petr93@gmail.com>
2020-04-26 18:00:41 +0200

    Change component to function

--- src/screens/TasksScreen.js
+++ src/screens/TasksScreen.js
@@ -2,7 +2,7 @@
  * @module Tasks
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Tasks from '../components/List/Tasks';
 import Header from '../components/Image/Header';
@@ -12,32 +12,22 @@ import styles from './Screen.module.scss';
 import image from '../assets/img/header.svg';
 
 /**
- * Function displays Tasker screen.
+ * Function displays Tasks screen.
  */
-class TasksScreen extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { timePeriod: getWeekDates() };
-	}
+function TasksScreen(props) {
+	const [timePeriod, setTimePeriod] = useState(getWeekDates());
 
-	handleTimePeriodChange = (timePeriod) => {
-		this.setState({ timePeriod: timePeriod });
-	};
-
-	render() {
-		return (
-			<div className={styles.layout}>
-				<Header image={image} alt="Header" text={this.props.location.state.alias} />
-				<Weeks timePeriod={this.handleTimePeriodChange} />
-				<Tasks userId={this.props.location.state.id} timePeriod={this.state.timePeriod} />
-			</div>
-		);
-	}
+	return (
+		<div className={styles.layout}>
+			<Header image={image} alt="Header" text={props.location.state.alias} />
+			<Weeks timePeriod={setTimePeriod} />
+			<Tasks userId={props.location.state.id} timePeriod={timePeriod} />
+		</div>
+	);
 }
 
 TasksScreen.propTypes = {
 	location: PropTypes.any,
-	match: PropTypes.object,
 };
 
 export default TasksScreen;
